a7af99163d1ab04fca3b845bc011ce642cf56ebd
eduardo mancilla <eduardo.mancilla@upax.com.mx>
2019-09-08 21:45:59 -0500

    Initial Commit

---
+++ ios-RecordAudioExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-RecordAudioExample
+//
+//  Created by eduardo mancilla on 9/8/19.
+//  Copyright © 2019 black. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
